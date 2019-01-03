e23808d55b9ed657cf4a16ab035289815290bb8f
XUCHENGZHUO <912342675@qq.com>
2019-01-03 21:51:07 +0800

    添加了space和assign指令

--- src/com/seu/Minisys32Assembler/FakeIns.java
+++ src/com/seu/Minisys32Assembler/FakeIns.java
@@ -5,6 +5,41 @@ import java.util.Vector;
 
 public class FakeIns {
 
+    private static Integer nextSize;
+
+    /**
+     * 伪指令处理
+     *
+     * @param dataDef 伪指令,必须全部是小写且首尾没有空格
+     * @return 以数组的形式返回字节流
+     * @throws Exception 伪指令格式错误
+     */
+    public static Vector<Byte> transDataDefine(String dataDef) throws Exception {
+        if (dataDef.startsWith(".space")) {
+            try {
+                return space(Integer.parseInt(dataDef.substring(".space".length())));
+            } catch (NumberFormatException e) {
+                throw new Exception("Data define error - Space must be positive number");
+            }
+        }
+
+        if (dataDef.startsWith(".align")) {
+            try {
+                align(Integer.parseInt(dataDef.substring(".align".length())));
+            } catch (NumberFormatException e) {
+                throw new Exception("Data define error - Bits must be greater than one");
+            }
+            return new Vector<>();
+        }
+
+        Vector<Byte> bytes = defines(dataDef);
+        while (bytes.size() < nextSize)
+            bytes.add((byte) 0);
+        nextSize = null;
+        return bytes;
+
+    }
+
     /**
      * 将数据定义伪指令翻译成数据字节
      * 小端存储
@@ -13,7 +48,7 @@ public class FakeIns {
      * @return 以数组的形式返回字节流
      * @throws Exception 伪指令格式错误
      */
-    public static Vector<Byte> transDataDefine(String dataDef) throws Exception {
+    private static Vector<Byte> defines(String dataDef) throws Exception {
 
         Vector<Byte> bytes = new Vector<>();
 
@@ -106,5 +141,19 @@ public class FakeIns {
         }
 
         return bytes;
+    }
+
+    private static Vector<Byte> space(int size) throws Exception {
+        if (size <= 0) throw new Exception();
+        Vector<Byte> bytes = new Vector<>();
+        for (int i = 0; i < size; i++) {
+            bytes.add((byte) 0);
+        }
+        return bytes;
+    }
+
+    private static void align(int numOfBits) throws Exception {
+        if (numOfBits <= 1) throw new Exception();
+        nextSize = 1 << numOfBits;
     }
 }
