5b6ae58d275d80f01bbfd1c06325e6f54ca825e5
zhangbingxi <1049477107@qq.com>
2019-01-03 21:34:38 +0800

    测试伪指令(成功)

--- src/test/seu/Minisys32Assembler/FakeInsTest.java
+++ src/test/seu/Minisys32Assembler/FakeInsTest.java
@@ -3,6 +3,8 @@ package test.seu.Minisys32Assembler;
 import com.seu.Minisys32Assembler.FakeIns;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Vector;
 
 public class FakeInsTest {
@@ -10,13 +12,40 @@ public class FakeInsTest {
     @Test
     public void transDataDefine() {
         try {
+            StringBuilder fact = new StringBuilder();
+            StringBuilder expect = new StringBuilder();
+            Vector<String> dataDefs = new Vector<>();
+            dataDefs.add(".word  0x000000ff, 0x55005500");
+            dataDefs.add(".byte 7,15,-3,0");
+            dataDefs.add(".half 20,70,-15,67");
+            dataDefs.add(".word 100,-50");
+            dataDefs.add(".word 0x10002000");
+            dataDefs.add(".half 0x77c,0");
+            dataDefs.add(".word 0xd");
+            dataDefs.add(".ascii \"hello\0\",\"a\"");
+            dataDefs.add(".asciiz \"hello\"");
+            dataDefs.add(".byte 6:3,4:2,6");
 
-            String dataDef = ".WORD  0X000000FF, 0X55005500".toLowerCase();
-            Vector<Byte> bytes = FakeIns.transDataDefine(dataDef);
-            for (byte b : bytes) {
-                System.out.print(Integer.toHexString(b & 0xff | 0xffffff00).substring(6) + "\t");
+            expect.append("ff00000000550055");
+            expect.append("070ffd00");
+            expect.append("14004600f1ff4300");
+            expect.append("64000000ceffffff");
+            expect.append("00200010");
+            expect.append("7c070000");
+            expect.append("0d000000");
+            expect.append("68656c6c6f0061");
+            expect.append("68656c6c6f00");
+            expect.append("060606040406");
+
+            for (String dataDef : dataDefs) {
+                Vector<Byte> bytes = FakeIns.transDataDefine(dataDef);
+
+                for (byte b : bytes) {
+                    fact.append(Integer.toHexString(b & 0xff | 0xffffff00).substring(6));
+                    //System.out.print(Integer.toHexString(b & 0xff | 0xffffff00).substring(6) + "\t");
+                }
             }
-            System.out.print("\n");
+            assertEquals(expect.toString(), fact.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
